d235f210dde7a819e6c7b2a5f1169a25be422893
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-04-26 21:59:04 +0200

    Attempt to raise volume as well as voice-frequency-levels (remove muddiness)

--- produce-episode.sh
+++ produce-episode.sh
@@ -17,7 +17,7 @@ mkdir .artifacts
 rm -rf .artifacts/* 
 
 
-LOG "(1/8) STITCHING"
+LOG "(1/7) STITCHING"
 ffmpeg -i alex-1.flac -i alex-2.flac \
 -filter_complex '[0:0][1:0]concat=n=2:v=0:a=1[out]' \
 -map '[out]' .artifacts/alex-stitched.flac &
@@ -32,35 +32,36 @@ ffmpeg -i sb-1.flac -i sb-2.flac \
 
 wait
 
-LOG "(2/8) NORMALIZE VOICES"
-ffmpeg-normalize .artifacts/alex-stitched.flac .artifacts/seb-stitched.flac -tp -1 -of .artifacts -ext flac -c:a flac --dual-mono -f --progress
-
-
-LOG "(3/8) PRE NORMALIZER COMPRESSOR/LIMITER/GAINER"
-ffmpeg -y -i .artifacts/alex-stitched.flac -filter_complex "compand=points=-40/-900|-35/-30|-30/-20|-25/-19|-20/-15|-15/-10|-10/-7|0/-1|20/-1" .artifacts/alex-compressed.flac &
-ffmpeg -y -i .artifacts/seb-stitched.flac -filter_complex  "compand=points=-40/-900|-35/-30|-30/-20|-25/-19|-20/-15|-15/-10|-10/-7|0/-1|20/-1" .artifacts/seb-compressed.flac &
+LOG "(2/7) HIGH-LOW VOICE PASS"
+ffmpeg -y -i .artifacts/alex-stitched.flac -af highpass=110,lowpass=7500 -ar 44100 .artifacts/alex-passes.flac &
+ffmpeg -y -i .artifacts/seb-stitched.flac -af highpass=125,lowpass=6500 -ar 44100 .artifacts/seb-passes.flac &
 wait
 
-LOG "(4/8) HIGH-LOW VOICE PASS"
-ffmpeg -y -i .artifacts/alex-compressed.flac -af highpass=110,lowpass=7500 -ar 44100 .artifacts/alex-passes.flac &
-ffmpeg -y -i .artifacts/seb-compressed.flac -af highpass=125,lowpass=6000 -ar 44100 .artifacts/seb-passes.flac &
+
+LOG "(3/7) NORMALIZE VOICES"
+ffmpeg-normalize .artifacts/alex-passes.flac .artifacts/seb-passes.flac -tp 0 -of .artifacts -ext flac -c:a flac --dual-mono -f --progress
+
+
+LOG "(4/7) PRE NORMALIZER COMPRESSOR/LIMITER/GAINER"
+ffmpeg -y -i .artifacts/alex-passes.flac -filter_complex "compand=points=-40/-900|-35/-25|-30/-18|-25/-16|-20/-13|-15/-10|-10/-5|0/0|20/0" .artifacts/alex-compressed.flac &
+ffmpeg -y -i .artifacts/seb-passes.flac -filter_complex  "compand=points=-40/-900|-35/-25|-30/-18|-25/-16|-20/-13|-15/-10|-10/-5|0/0|20/0" .artifacts/seb-compressed.flac &
 wait
 
-LOG "(5/8) NORMALIZE SOUNDBOARD"
+LOG "(5/7) NORMALIZE SOUNDBOARD"
 ffmpeg-normalize .artifacts/sb-stitched.flac -o .artifacts/sb.flac --normalization-type peak --target-level -4 -c:a flac -f --progress
 
 
-LOG "(6/8) MERGE ALL"
-sox -M .artifacts/sb.flac .artifacts/alex-passes.flac .artifacts/seb-passes.flac .artifacts/all.flac remix -m -p 1,3,4 2,3,4
+LOG "(6/7) MERGE ALL"
+sox -M .artifacts/sb.flac .artifacts/alex-compressed.flac .artifacts/seb-compressed.flac .artifacts/all.flac remix -m -p 1,3,4 2,3,4
 
 
-LOG "(7/8) DOWNWARD COMPRESSION"
-ffmpeg -y -i .artifacts/all.flac  -af \
-  acompressor=threshold=0.9:ratio=3:attack=15:release=100 \
-  $1.flac
+# LOG "(7/7) DOWNWARD COMPRESSION"
+# ffmpeg -y -i .artifacts/all.flac  -af \
+#   acompressor=threshold=0.9:ratio=3:attack=15:release=100 \
+#   $1.flac
 
-LOG "(8/8) PRODUCE MP3"
-ffmpeg -y -i $1.flac -vn -ar 44100 -ac 2 -ab 192k -f mp3 -joint_stereo 1 "../../Finished Episodes/$1-192.mp3"
+LOG "(7/7) PRODUCE MP3"
+ffmpeg -y -i .artifacts/all.flac -vn -ar 44100 -ac 2 -ab 192k -f mp3 -joint_stereo 1 "../../Finished Episodes/$1-192.mp3"
 
 
 end_time=`date +%s`
