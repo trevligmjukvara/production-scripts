9366491f9f980b0cb34a78c62c2ee1cf3888bf73
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-11-05 10:22:23 +0100

    Fetch tag inside add_chapters instead

--- produce-chapters.py
+++ produce-chapters.py
@@ -12,10 +12,12 @@ import sys
 from docopt import docopt
 from tinytag import TinyTag  # for flac length parsing
 
+
 class Chapter:
     start = 0
     end = 0
     title = "title"
+
 
 def parse_chapters_file(fname, offset_ms):
     filename = os.path.splitext(fname)
@@ -26,13 +28,15 @@ def parse_chapters_file(fname, offset_ms):
             timeMs = int((float(time)*1000) + offset_ms)
             chap = Chapter()
             chap.start = timeMs
-            chap.title = title;
+            chap.title = title
             chaps.append(chap)
-            print("{} seconds in: '{}'".format( timeMs/1000, title))
+            print("{} seconds in: '{}'".format(timeMs/1000, title))
     return chaps
 
 
-def add_chapters(tag, fname, chaps):
+def add_chapters(fname, chaps):
+    tag = Tag()
+    tag.parse(target_mp3)
     audioFile = core.load(fname)
     total_length = audioFile.info.time_secs * 1000
     tag.setTextFrame(b"TLEN", str(int(total_length)))
@@ -40,13 +44,13 @@ def add_chapters(tag, fname, chaps):
         if i < (len(chaps)-1):
             chap.end = chaps[i+1].start
 
-    chaps[-1].end = total_length;
+    chaps[-1].end = total_length
 
     index = 0
     child_ids = []
     for chap in chaps:
         element_id = "ch{}".format(index).encode()
-        print ("Adding chapter {} at {}".format(chap.title,chap.start))
+        print("Adding chapter {} at {}".format(chap.title, chap.start))
         new_chap = tag.chapters.set(element_id, (chap.start, chap.end))
         new_chap.sub_frames.setTextFrame(b"TIT2", u"{}".format(chap.title))
         child_ids.append(element_id)
@@ -55,11 +59,13 @@ def add_chapters(tag, fname, chaps):
     list_chaps(tag)
     tag.save()
 
+
 def list_chaps(tag):
-  "list chapters in tag"
-  print("Chapters:")
-  for chap in tag.chapters:
-    print(chap.sub_frames.get(b"TIT2")[0]._text)
+    "list chapters in tag"
+    print("Chapters:")
+    for chap in tag.chapters:
+        print(chap.sub_frames.get(b"TIT2")[0]._text)
+
 
 def main():
     "Entry point"
@@ -69,15 +75,13 @@ def main():
     chapters_p1_file = sys.argv[2]
     chapters_p2_file = sys.argv[3]
     target_mp3 = sys.argv[4]
-    tag = Tag()
-    tag.parse(target_mp3)
 
     chapters = []
     chapters += parse_chapters_file(chapters_p1_file, 0)
     chapters += parse_chapters_file(chapters_p2_file, part1_length)
-    print (len(chapters))
+    print(len(chapters))
 
-    add_chapters(tag, target_mp3, chapters)
+    add_chapters(target_mp3, chapters)
 
 
 if __name__ == '__main__':
